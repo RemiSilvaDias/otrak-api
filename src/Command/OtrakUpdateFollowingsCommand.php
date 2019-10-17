386c801c3cc698cd9bd0d68dc5afd246fb42f016
Cedric Anthony <dev+git@anthodev.net>
2019-10-17 16:41:15 +0200

    Added console command to update the tracking of the shows of the users

---
+++ src/Command/OtrakUpdateFollowingsCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Command;
+
+use App\Controller\ApiController;
+use App\Repository\UserRepository;
+use App\Repository\FollowingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class OtrakUpdateFollowingsCommand extends Command
+{
+    public const STATUS_IN_DEVELOPMENT = 0;
+    public const STATUS_RUNNING = 1;
+    public const STATUS_ENDED = 2;
+    
+    public const TRACKING_WATCHING = 0;
+    public const TRACKING_COMPLETED = 1;
+    public const TRACKING_SEE_NEXT = 2;
+    public const TRACKING_UPCOMING = 3;
+    public const TRACKING_STOPPED = 4;
+    
+    protected static $defaultName = 'otrak:update:followings';
+
+    private $userRepository;
+    private $followingRepository;
+    private $em;
+
+    public function __construct(EntityManagerInterface $em, UserRepository $userRepository, FollowingRepository $followingRepository)
+    {
+        $this->userRepository = $userRepository;
+        $this->followingRepository = $followingRepository;
+        $this->em = $em;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Update the tracking status of the shows of the users')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        // $arg1 = $input->getArgument('arg1');
+
+        // if ($arg1) {
+        //     $io->note(sprintf('You passed an argument: %s', $arg1));
+        // }
+
+        // if ($input->getOption('option1')) {
+        //     // ...
+        // }
+
+        $users = $this->userRepository->findAll();
+
+        foreach ($users as $user) {
+            $userFollowings = $this->followingRepository->findBy(['user' => $user, 'season' => null, 'episode' => null]);
+
+            foreach ($userFollowings as $following) {
+                if ($following->getStatus() == self::TRACKING_UPCOMING || $following->getStatus() == self::TRACKING_COMPLETED) {
+                    if ($following->getTvShow()->getStatus() == self::STATUS_RUNNING && !is_null($following->getTvShow()->getSeasons()->last()->getEpisodes()->first()->getAirstamp()) && $following->getTvShow()->getSeasons()->last()->getEpisodes()->first()->getAirstamp() < new \DateTime()) {
+                        $following->setStatus(self::TRACKING_WATCHING);
+                        $this->em->persist($following);
+                    }
+                }
+            }
+        }
+
+        $this->em->flush();
+
+        $io->success('The trackings have been updated.');
+    }
+}
