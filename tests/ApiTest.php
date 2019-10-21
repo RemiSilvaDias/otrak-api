11b5878a08cffcb1e56377e4f785770f449934ba
Cedric Anthony <dev+git@anthodev.net>
2019-10-21 12:11:27 +0200

    Added first round of tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -2,32 +2,81 @@
 
 namespace App\Tests;
 
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiTest extends WebTestCase
 {
-    public function testSearch($term)
+    protected function createAuthenticatedClient($email = 'admin@oc.io', $password = 'admin')
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/login_check',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            json_encode([
+                'email' => $email,
+                'password' => $password,
+            ])
+        );
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $client = static::createClient();
+        $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+
+        return $client;
+    }
+
+    public function testSearch()
     {
         $client = self::createClient();
-        $client->request('GET', '/shows/search/' . $term);
+        $client->request('GET', 'http://localhost:8001/api/shows/search/game');
 
         $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
     public function testAired()
     {
-        $client = self::createClient();
-        $client->request('GET', '/shows/aired');
+        $client = static::createClient();
+        $client->request('GET', 'http://localhost:8001/api/shows/aired');
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function testLogin()
+    {
+        $client = $this->createAuthenticatedClient();
+        $client->request('GET', 'http://localhost:8001/api/users/profile');
 
         $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
     public function testNextShows()
     {
-        $client = self::createClient();
-        $client->request('GET', '/shows/next');
+        $client = static::createClient();
+        $client->request('GET', 'http://localhost:8001/api/shows/next');
 
-        $this->assertEquals(410, $client->getResponse()->getStatusCode());
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+
+        // Test "Next shows" for connected user
+        $client = $this->createAuthenticatedClient();
+        $client->request('GET', 'http://localhost:8001/api/shows/next');
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function testNewFollowing()
+    {
+        $client = $this->createAuthenticatedClient();
+        $client->request('GET', 'http://localhost:8001/api/users/profile');
+
+        $userId = \json_decode($client->getResponse()->getContent())->id;
+
+        $client->request('POST', "http://localhost:8001/api/followings/new/{$userId}/0/666/1/1");
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 }
